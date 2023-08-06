4bf6c1403e31fbf276a4611243d03468211836c1
Michal Hicz <michal07.h@gmail.com>
2023-08-06 08:18:17 +0200

    Update printCommand.py

--- printCommand.py
+++ printCommand.py
@@ -3,7 +3,11 @@ import subprocess
 import sys
 import time
 
-lv = "forge-1.20.1-47.1.43"
+
+# Dont forget to update version variabile to your version!!!
+
+
+version = "forge-1.20.1-47.1.43"
 md = "."
 options = {
     "username": "replaceme",
@@ -11,7 +15,7 @@ options = {
     "token": "-"
 }
 
-minecraft_command = minecraft_launcher_lib.command.get_minecraft_command(lv, md, options)
+minecraft_command = minecraft_launcher_lib.command.get_minecraft_command(version, md, options)
 minecraft_command.insert(3, '-Dminecraft.launcher.brand=mikiho-skurveny-launcher')
 minecraft_command[0] = 'jvm/java-runtime-gamma/bin/javaw.exe'
 
